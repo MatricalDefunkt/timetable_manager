afd978b305cb658788613319be8a0c4cd03287cb
MatricalDefunkt <pramitsmusic@gmail.com>
2023-08-24 16:45:17 +0530

    Added a dotenv file with an example file

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
 import { DataTypes, Model, Sequelize } from "sequelize";
+import { config } from "dotenv";
 
-const sequelize = new Sequelize("timetable_manager", "root", "Baku@2010", {
-  host: "localhost",
-  dialect: "mysql",
-  port: 8080,
-});
+config();
+
+if (!process.env.DB_ROOT_PASSWORD || !process.env.DB_PORT) {
+  throw new Error("DB_ROOT_PASSWORD or DB_PORT is not defined");
+}
+
+const sequelize = new Sequelize(
+  "timetable_manager",
+  "root",
+  process.env.DB_ROOT_PASSWORD,
+  {
+    host: "localhost",
+    dialect: "mysql",
+    port: Number(process.env.DB_PORT),
+  }
+);
 
 class Teachers extends Model<{
   id: number | null;
