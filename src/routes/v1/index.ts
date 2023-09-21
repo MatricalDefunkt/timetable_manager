d6e3ccc607caa2a60cfb30d00a724d296f6f6e01
MatricalDefunkt <pramitsmusic@gmail.com>
2023-09-21 05:46:08 +0000

    Made a generic api

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,44 +1,104 @@
 import Elysia from "elysia";
-import databases from "../../database";
+import databases from "../../models";
+import { ModelStatic } from "sequelize";
 
 const plugin = new Elysia();
 
-plugin.group("/v1", (plugin) => {
-  return plugin
-    .get("/", ({ set }) => {
-      set.status = 200;
-      return {
-        message: "Hello, api!",
-      };
-    })
-    .get("/teachers", async ({ query, set, headers }) => {
-      console.log(headers);
-      if (
-        !headers.authorization ||
-        (headers.authorization.startsWith("Bearer") &&
-          headers.authorization.split(" ")[1] == Bun.env.BEARER_TOKEN)
-      ) {
-        set.status = 401;
+const { Batches, Classrooms, Divisions, Slots, Subjects, Teachers } = databases;
+
+const createRoutesForDatabase = (
+  database: ModelStatic<any>,
+  baseUrl: string
+) => {
+  const resourceName = baseUrl.split("/").pop();
+
+  plugin.group("/v1/", (plugin) => {
+    return plugin
+      .get(`/${resourceName}`, async ({ query, set, headers }) => {
+        if (
+          !headers.authorization ||
+          (headers.authorization.startsWith("Bearer") &&
+            headers.authorization.split(" ")[1] == Bun.env.BEARER_TOKEN)
+        ) {
+          set.status = 401;
+          return {
+            message: "Unauthorized",
+          };
+        }
+        if (query.q == "all") {
+          set.status = 200;
+          return await database.findAll();
+        } else if (query.q) {
+          const resource = await database.findByPk(String(query.q));
+          if (resource) {
+            set.status = 200;
+            return resource;
+          } else {
+            set.status = 404;
+            return {
+              message: "Not Found",
+            };
+          }
+        } else {
+          set.status = 400;
+          return {
+            message: "Bad Request",
+          };
+        }
+      })
+      .post(`/${resourceName}`, async ({ body, set, headers }) => {
+        if (
+          !headers.authorization ||
+          (headers.authorization.startsWith("Bearer") &&
+            headers.authorization.split(" ")[1] == Bun.env.BEARER_TOKEN)
+        ) {
+          set.status = 401;
+          return {
+            message: "Unauthorized",
+          };
+        }
+        if (typeof body != "object") {
+          set.status = 400;
+          return {
+            message: "Bad Request",
+          };
+        }
+
+        const newResource = await database
+          .create({
+            ...body,
+          })
+          .catch((err) => {
+            set.status = 400;
+            return {
+              message: err.message,
+            };
+          });
+        set.status = 201;
+        return newResource;
+      })
+      .get("*", ({ set }) => {
+        set.status = 403;
         return {
-          message: "Unauthorized",
+          message: "Forbidden",
         };
-      }
-      if (query.q == "all") {
-        set.status = 200;
-        return await databases.Classroom.findAll();
-      } else {
-        set.status = 400;
-        return {
-          message: "Bad Request",
-        };
-      }
-    })
-    .get("*", ({ set }) => {
-      set.status = 403;
-      return {
-        message: "Forbidden",
-      };
-    });
+      });
+  });
+};
+
+// Assuming you have an array of databases to create routes for
+const databasesList: { name: string; database: ModelStatic<any> }[] = [
+  { name: "Teachers", database: Teachers },
+  { name: "Classrooms", database: Classrooms },
+  { name: "Divisions", database: Divisions },
+  { name: "Slots", database: Slots },
+  { name: "Subjects", database: Subjects },
+  { name: "Batches", database: Batches },
+];
+
+// Create routes for each database in the list
+databasesList.forEach(({ name, database }) => {
+  createRoutesForDatabase(database, `/v1/${name.toLowerCase()}`);
 });
 
 export default plugin;
