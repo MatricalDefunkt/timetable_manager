56c239ba9e9af7e22b4595f10687b74bc7201dad
MatricalDefunkt <pramitsmusic@gmail.com>
2023-09-21 05:49:07 +0000

    Created template logic for slot creation

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,39 @@
-import { Elysia } from "elysia";
-import plugin from "./routes/v1";
+import Models from "./models"; // Import your Sequelize models
 
-const app = new Elysia();
+const { Batches, Classrooms, Divisions, Slots, Subjects, Teachers } = Models;
 
-app.get("/", ({ set }) => {
-  set.status = 200;
-  return {
-    message: "Hello, Prishita!",
-  };
-});
+export async function timetableGenerator(
+  subjectId: string,
+  slotsPerDay: number,
+  durationPerSlot: number
+) {
+  try {
+    // Implement your timetable generation logic here
+    // This is a simplified example, you'll need to replace this with your actual algorithm
 
-app.use(plugin);
-app.listen(3000);
+    const timetable = []; // Array to store timetable slots
 
-console.log("Server is running on port 3000");
+    for (let i = 1; i <= slotsPerDay; i++) {
+      const slot = await Slots.create();
+
+      const teacher = await Teachers.findOne({ where: { id: 1 } }); // Replace with actual teacher selection logic
+      const classroom = await Classrooms.findOne({ where: { id: 1 } }); // Replace with actual classroom selection logic
+      const subject = await Subjects.findOne({ where: { id: subjectId } }); // Replace with actual subject selection logic
+
+      if (!teacher || !classroom || !subject)
+        throw new Error(
+          "Could not find requested teacher, classroom or subject"
+        );
+
+      slot.teacherid = teacher.id;
+      slot.classroomid = classroom.id;
+      slot.subjectid = subject.id;
+
+      timetable.push(slot);
+    }
+
+    return timetable;
+  } catch (error) {
+    throw new Error("Error generating timetable: " + error);
+  }
+}
